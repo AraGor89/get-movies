f1a1c03783f7179dfa6411950d33179f02bfb56f
GorAraqelyan <50023373+GorAraqelyan@users.noreply.github.com>
2020-06-22 17:08:12 +0400

    Updates

---
+++ precache-manifest.313b589d165f403f83de257ec72927df.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9369150fda75263c3fe94bbef10de3cb",
+    "url": "/get-movies/index.html"
+  },
+  {
+    "revision": "56ff8007bb85f2c8c079",
+    "url": "/get-movies/static/css/main.3fcd11d6.chunk.css"
+  },
+  {
+    "revision": "441b690d4fce07169525",
+    "url": "/get-movies/static/js/2.9785a40e.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/get-movies/static/js/2.9785a40e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "56ff8007bb85f2c8c079",
+    "url": "/get-movies/static/js/main.e7959bce.chunk.js"
+  },
+  {
+    "revision": "8c897783d1e7cb78d96b",
+    "url": "/get-movies/static/js/runtime-main.63a75da4.js"
+  }
+]);
